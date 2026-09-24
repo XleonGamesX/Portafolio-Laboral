f70886ee57d61c45bca5aaec7a6a7c18a1ed482c
XleonGamesX <santiagoleon0407@gmail.com>
2026-09-23 20:17:16 -0500

    Cambio en los estudios

--- src/data/education.js
+++ src/data/education.js
@@ -3,11 +3,15 @@ export const education = [
     title: 'Bachiller con especialidad en Gestión Contable'
   },
   {
+    title: 'Tecnologo en Analisisi y desarrollo de sofwatere',
+    institution: 'SENA'
+  },
+  {
     title: 'Técnico en Contabilización de Operaciones Comerciales y Mercantiles',
     institution: 'SENA'
   },
   {
-    title: 'Técnico en Manipulación de Alimentos',
+    title: 'Curso en Manipulación de Alimentos',
     institution: 'SENA'
   }
 ]
